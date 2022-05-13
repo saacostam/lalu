bcd1f462377b198091339511318dadfad71a147f
saacostam <saacostam@unal.edu.co>
2022-05-13 15:16:41 -0500

    first-commit

---
+++ src/components/Landing/Carousel.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="carousel">
+        <div id="current-image" :style="{'background-image':`url(${this.links[this.current]})`}"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data:()=>{
+        return {
+            links:[
+                'https://picsum.photos/2040/1080',
+                'https://picsum.photos/2040/1081',
+                'https://picsum.photos/2040/1082'
+            ],
+            current:0,
+            timeOut: 5000
+        }
+    },
+    methods:{
+        updateImage(){
+            this.current = (this.current+1)%this.links.length;
+            setTimeout(() => this.updateImage(), this.timeOut);
+        }
+    },
+    beforeMount(){
+        this.updateImage();
+    }
+}
+</script>
+
+<style scoped>
+#current-image{
+    width: 100%;
+    height: 100%;
+    background-size:cover;
+    background-position: center;
+    transition: ease-in background-image;
+}
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="landing">
+    <Carousel class="carousel"/>
+    <div class="forms">
+
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Carousel from '@/components/Landing/Carousel.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Carousel
+  }
+}
+</script>
+
+<style scoped>
+.landing{
+  width: 100vw;
+  height: 100vh;
+  background-color: #101010;
+
+  display: flex;
+}
+
+.carousel, .forms{
+  flex: 1;
+}
+</style>
