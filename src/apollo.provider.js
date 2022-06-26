a9339afa91d1d7eefdd86f358b019a90c2c65fec
saacostam <saacostam@unal.edu.co>
2022-06-26 16:19:40 -0500

    graphql-ip

--- src/apollo.provider.js
+++ src/apollo.provider.js
@@ -5,7 +5,7 @@ const cache = new InMemoryCache();
 
 const apolloClient = new ApolloClient({
     cache,
-    uri: "http://lalu-ag:3007/graphql",
+    uri: "http://10.252.5.109:3007/graphql",
 });
 
 export const provider = createApolloProvider({
