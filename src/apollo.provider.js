cad663c6a0dc10b1efe4d4d6e0f11a16dbd1b5b4
saacostam <saacostam@unal.edu.co>
2022-06-26 15:58:11 -0500

    added port

--- src/apollo.provider.js
+++ src/apollo.provider.js
@@ -5,7 +5,7 @@ const cache = new InMemoryCache();
 
 const apolloClient = new ApolloClient({
     cache,
-    uri: "http://10.252.5.109:3007/graphql",
+    uri: "http://lalu-wa:3007/graphql",
 });
 
 export const provider = createApolloProvider({
