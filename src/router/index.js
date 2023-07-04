54cc4b7f682457be748b26c80b9a525695d8e6a4
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2023-07-04 02:48:39 -0500

    change history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Landing from '../views/Landing.vue'
 import Auth from '../views/Auth.vue'
 import NotFound from '../views/NotFound.vue'
@@ -66,7 +66,7 @@ const routes = [{
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes
 })
 
